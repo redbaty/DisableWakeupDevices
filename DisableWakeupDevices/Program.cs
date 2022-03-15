bbada11378aba2b21d525fedd930b1159d1a36fe
Marcos Cordeiro <marcos.cordeiro@andbank.com.br>
2022-03-14 21:40:53 -0300

    Code refactoring
    Support for "all" devices selection using 'a'

--- DisableWakeupDevices/Program.cs
+++ DisableWakeupDevices/Program.cs
@@ -5,187 +5,191 @@ using System.Drawing;
 using System.Linq;
 using Console = Colorful.Console;
 
-namespace DisableWakeupDevices
+namespace DisableWakeupDevices;
+
+internal static class Program
 {
-    static class Program
+    private static List<string> ArmedDevices { get; } = GetArmedDevices().ToList();
+
+    private static List<string> Devices { get; } = GetDevices().ToList();
+
+    private static void Main()
     {
-        public static List<string> ArmedDevices { get; } = GetArmedDevices().ToList();
-        public static List<string> Devices { get; } = GetDevices().ToList();
+        var response = AskQuestion("Do you wish to (e)nable or (d)isable devices ?", new List<string> { "e", "d" });
 
-        static void Main()
+        if (string.Equals(response, "e", StringComparison.InvariantCultureIgnoreCase))
+            EnableDevices();
+        else
+            DisableDevices();
+
+        Console.WriteLine("Press any key to close this window.");
+        Console.ReadKey();
+    }
+
+    private static string AskQuestion(string question, IReadOnlyCollection<string> possibleinputs)
+    {
+        while (true)
         {
-            Console.WriteAscii("Devices", Color.CornflowerBlue);
-            Console.WriteLine("Management v1.0\n", Color.YellowGreen);
-
-            var response = AskQuestion("Do you wish to (e)nable or (d)isable devices ?", new List<string> {"e", "d"});
-
-            if (response == "e")
-                EnableDevices();
-            else
-                DisableDevices();
-
-            Console.WriteLine("Press any key to close this window.");
-            Console.ReadKey();
+            Console.Write("> " + question + " ");
+            var response = Console.ReadLine().Trim();
+            if (possibleinputs.Any(p => string.Equals(p, response, StringComparison.InvariantCultureIgnoreCase))) return response;
+            Console.WriteLine("Invalid input.");
         }
+    }
 
-        static string AskQuestion(string question, List<string> possibleinputs)
+    private static void EnableDevices()
+    {
+        Console.WriteLine("Devices that can wake up your computer: ");
+        
+        foreach (var device in Devices.OrderBy(i => i))
         {
-            while (true)
-            {
-                Console.Write("> " + question + " ");
-                var response = Console.ReadLine().Trim().ToLower();
-                if (possibleinputs.Any(p => p == response))
-                {
-                    return response;
-                }
-                Console.WriteLine("Invalid input.");
-            }
+            var index = Devices.IndexOf(device);
+            Console.WriteLine($"[{index:00}] {device}");
         }
-
-        private static void EnableDevices()
+        
+        while (true)
         {
-            Console.WriteLine("Devices that can wake up your computer: ");
-            for (var index = 0; index < Devices.Count; index++)
-            {
-                var device = Devices[index];
-                Console.WriteLine($"[{index}] {device}");
-            }
-
-            Console.Write("\n> Type in the devices you want to enable, separated by a comma: ");
-            var enabledevices = Console.ReadLine().Split(',').ToDeviceName().ToList();
-
+            Console.Write("\n> Type in the devices you want to enable, separated by a comma or type 'a' to enable all of them:: ");
+            if (!Console.ReadLine().Split(',').ToDeviceName(out var enabledevices)) continue;
+            
             Console.WriteLine("> Are you sure that you want to enable the following devices ?\n");
-            foreach (var device in enabledevices)
-            {
-                Console.WriteLine($": {device}", Color.Gold);
-            }
+            foreach (var device in enabledevices) Console.WriteLine(device, Color.Gold);
 
-            WaitForYes();
+            YesOrExit();
             EnableDevice(enabledevices);
+            break;
         }
+    }
 
-        private static void WaitForYes()
+    private static void YesOrExit()
+    {
+        while (true)
         {
-            while (true)
-            {
-                Console.Write("\n> [(y)es/(n)o]: ");
-                var response = Console.ReadLine().Trim().ToLower();
-                if (response == "n")
-                {
-                    Environment.Exit(0);
-                }
-                if (response == "y")
-                {
-                    break;
-                }
-                Console.WriteLine("Sorry, please type in only the characters Y for yes or N for no.");
-            }
+            Console.Write("\n> [(y)es/(n)o]: ");
+            var response = Console.ReadLine().Trim();
+            if (string.Equals(response, "n", StringComparison.InvariantCultureIgnoreCase)) Environment.Exit(0);
+            if (string.Equals(response, "y", StringComparison.InvariantCultureIgnoreCase)) break;
+            Console.WriteLine("Sorry, please type in only the characters Y for yes or N for no.");
         }
+    }
 
-        private static void DisableDevices()
+    private static void DisableDevices()
+    {
+        if (ArmedDevices.Count < 1)
         {
-            Console.WriteLine("Devices that can wake up your computer: ");
-            for (var index = 0; index < ArmedDevices.Count; index++)
-            {
-                var device = ArmedDevices[index];
-                Console.WriteLine($"[{index}] {device}");
-            }
-
-            Console.Write("\n> Type in the devices you want to disable, separated by a comma: ");
-            var disabledevices = Console.ReadLine().Split(',').ToArmedDeviceName().ToList();
-
+            Console.WriteLine("No devices are currently able to wake up your computer", Color.Gold);
+            return;
+        }
+        
+        Console.WriteLine("Devices that can wake up your computer: ");
+        
+        foreach (var device in ArmedDevices.OrderBy(i => i))
+        {
+            var index = ArmedDevices.IndexOf(device);
+            Console.WriteLine($"[{index:00}] {device}");
+        }
+        
+        while (true)
+        {
+            Console.Write("\n> Type in the devices you want to disable, separated by a comma or type 'a' to disable all of them: ");
+            if (!Console.ReadLine().Split(',').ToArmedDeviceName(out var disabledevices)) continue;
+            
             Console.WriteLine("> Are you sure that you want to disable the following devices ?\n");
-            foreach (var disabledevice in disabledevices)
-            {
-                Console.WriteLine($": {disabledevice}", Color.Crimson);
-            }
+            foreach (var disabledevice in disabledevices) Console.WriteLine($": {disabledevice}", Color.Crimson);
 
-            WaitForYes();
+            YesOrExit();
             DisableDevice(disabledevices);
+            break;
         }
+    }
 
-        private static IEnumerable<string> ToArmedDeviceName(this IEnumerable<string> list) => list.Select(
-            item => ArmedDevices[Convert.ToInt32(item)]);
+    private static bool ToArmedDeviceName(this ICollection<string> list, out List<string> deviceNames) => ParseDeviceRange(list, ArmedDevices, out deviceNames);
+    
+    private static bool ParseDeviceRange(ICollection<string> list, ICollection<string> indexes, out List<string> deviceNames)
+    {
+        deviceNames = new List<string>();
 
-        private static IEnumerable<string> ToDeviceName(this IEnumerable<string> list) => list.Select(
-            item => Devices[Convert.ToInt32(item)]);
-
-        private static IEnumerable<string> GetDevices()
+        if (list.Count == 1 && string.Equals(list.ElementAt(0), "a", StringComparison.InvariantCultureIgnoreCase))
         {
-            var proc = new Process
+            deviceNames = indexes.ToList();
+            return true;
+        }
+        
+        foreach (var inputInt in list)
+        {
+            if (int.TryParse(inputInt, out var index))
             {
-                StartInfo =
+                if (indexes.ElementAtOrDefault(index) is { } deviceName)
                 {
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    FileName = "powercfg",
-                    Arguments = "/devicequery wake_from_any"
+                    deviceNames.Add(deviceName);
+                    continue;
                 }
-            };
-            proc.Start();
-            var output = proc.StandardOutput.ReadToEnd();
 
-            return output.Trim().Split('\n').ToList();
+                Console.WriteLine("Argument out of range", Color.Red);
+                return false;
+            }
+            
+            Console.WriteLine("Argument is not an integer", Color.Red);
+            return false;
         }
 
-        private static IEnumerable<string> GetArmedDevices()
+        return true;
+    }
+
+    private static bool ToDeviceName(this ICollection<string> list, out List<string> deviceNames) => ParseDeviceRange(list, Devices, out deviceNames);
+
+    private static string[] GetDevices() => QueryPowerCfg("/devicequery wake_from_any");
+
+    private static string[] QueryPowerCfg(string arguments)
+    {
+        using var proc = CreatePowerCfgProcess(arguments);
+        proc.Start();
+        var output = proc.StandardOutput.ReadToEnd();
+
+        return output.Trim().Split('\n');
+    }
+
+    private static Process CreatePowerCfgProcess(string arguments)
+    {
+        return new Process
         {
-            var proc = new Process
+            StartInfo =
             {
-                StartInfo =
-                {
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    FileName = "powercfg",
-                    Arguments = "/devicequery wake_armed"
-                }
-            };
-            proc.Start();
-            var output = proc.StandardOutput.ReadToEnd();
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                FileName = "powercfg",
+                Arguments = arguments
+            }
+        };
+    }
 
-            return output.Trim().Split('\n').ToList();
-        }
+    private static string[] GetArmedDevices()
+    {
+        var queryPowerCfg = QueryPowerCfg("/devicequery wake_armed");
+        return queryPowerCfg.Length == 1 && queryPowerCfg[0] == "NONE" ? Array.Empty<string>() : queryPowerCfg;
+    }
 
-        private static void DisableDevice(IEnumerable<string> devices)
-        {
-            foreach (var device in devices)
-                DisableDevice(device.Trim());
-        }
+    private static void DisableDevice(IEnumerable<string> devices)
+    {
+        foreach (var device in devices)
+            DisableDevice(device.Trim());
+    }
 
-        private static void DisableDevice(string device)
-        {
-            var proc = new Process
-            {
-                StartInfo =
-                {
-                    UseShellExecute = false,
-                    RedirectStandardOutput = false,
-                    FileName = "powercfg",
-                    Arguments = $"/devicedisablewake \"{device}\""
-                }
-            };
-            proc.Start();
-        }
+    private static void DisableDevice(string device)
+    {
+        using var proc = CreatePowerCfgProcess($"/devicedisablewake \"{device}\"");
+        proc.Start();
+    }
 
-        private static void EnableDevice(IEnumerable<string> devices)
-        {
-            foreach (var device in devices)
-                EnableDevice(device.Trim());
-        }
+    private static void EnableDevice(IEnumerable<string> devices)
+    {
+        foreach (var device in devices)
+            EnableDevice(device.Trim());
+    }
 
-        private static void EnableDevice(string device)
-        {
-            var proc = new Process
-            {
-                StartInfo =
-                {
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    FileName = "powercfg",
-                    Arguments = $"/deviceenablewake \"{device}\""
-                }
-            };
-            proc.Start();
-        }
+    private static void EnableDevice(string device)
+    {
+        using var proc = CreatePowerCfgProcess($"/deviceenablewake \"{device}\"");
+        proc.Start();
     }
 }
